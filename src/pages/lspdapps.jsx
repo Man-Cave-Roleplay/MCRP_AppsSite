fc13b446db9a02aa2f965422d52562a380277053
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-02-28 23:05:39 +0000

    Update lspdapps.jsx

--- src/pages/lspdapps.jsx
+++ src/pages/lspdapps.jsx
@@ -35,7 +35,7 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div>
-        <h1 style={{ color: 'white' }}>Department Head Applications</h1>
+        <h1 style={{ color: 'white' }}>LSPD Application</h1>
         <form
           name="lspdapps"
           method="post"
