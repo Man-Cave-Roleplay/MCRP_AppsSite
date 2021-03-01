1c3b254bab06df8d2c9fc55230be0d8263ebbc2d
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-01 21:52:55 +0000

    Update appeals.jsx

--- src/pages/appeals.jsx
+++ src/pages/appeals.jsx
@@ -54,14 +54,50 @@ export default class Contact extends React.Component {
           </p>
           <p style={{ color: 'white' }}>
             <label>
-              Your Discord Username and Tag| Ex: XG.Mattis#0001:<br />
+              Your Email:<br />
               <input type="text" style={{ width: '75%' }} name="name" onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
-              Reason For Appeal:<br />
+              Your Discord Username and Tag| Ex: XG.Mattis#0001:<br />
               <input type="id" style={{ width: '75%' }} style={{ width: '75%' }} name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+                <p style={{ color: 'white' }}>
+            <label>
+              Your Steam Hex Can Be Found Here http://www.vacbanned.com/:<br />
+              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p style={{ color: 'white' }}>
+            <label>
+             Why Was You Banned?:<br />
+              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p style={{ color: 'white' }}>
+            <label>
+              How Long Is Your Ban?:<br />
+              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+            </label>
+          </p>
+                <p style={{ color: 'white' }}>
+            <label>
+              If You Have Proof To Fight Against Your Ban And Prove Your Innocence Please State Here:<br />
+              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+            </label>
+          </p>
+                <p style={{ color: 'white' }}>
+            <label>
+             Discord Username Of Staff Who Banned You:<br />
+              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+            </label>
+          </p>
+                <p style={{ color: 'white' }}>
+            <label>
+              Do You Understand By Pressing Submit You Accept Our TOS/Rules And Promise To Follow Them If Unbanned:<br />
+              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p>
