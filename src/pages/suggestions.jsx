c51c761386f808eb487745a7a56440281a8121d5
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-01 14:27:37 +0000

    Update suggestions.jsx

--- src/pages/suggestions.jsx
+++ src/pages/suggestions.jsx
@@ -1,1 +1,87 @@
 
+import React from "react";
+import { navigateTo } from "gatsby-link";
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: 'white' }}>Suggestioins</h1>
+        <form
+          name="suggestions"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p style={{ color: 'white' }}>
+            <label>
+              Your Discord Username and Tag| Ex: XG.Mattis#0001:<br />
+              <input type="text" style={{ width: '75%' }} name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+                <p style={{ color: 'white' }}>
+            <label>
+              Your Email:<br />
+              <input type="id" style={{ width: '75%' }} style={{ width: '75%' }} name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+                <p style={{ color: 'white' }}>
+            <label>
+              Suggestion Reason:<br />
+              <input type="id" style={{ width: '75%' }} style={{ width: '75%' }} name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+                <p style={{ color: 'white' }}>
+            <label>
+              Suggestion Link:<br />
+              <input type="id" style={{ width: '75%' }} style={{ width: '75%' }} name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
