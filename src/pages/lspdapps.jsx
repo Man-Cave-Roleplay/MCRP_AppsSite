c9f7ad6d7840c02aaef4be5924ef0a0faf55b3b7
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-03 00:22:36 +0000

    Update lspdapps.jsx

--- src/pages/lspdapps.jsx
+++ src/pages/lspdapps.jsx
@@ -45,93 +45,93 @@ export default class Contact extends React.Component {
           onSubmit={this.handleSubmit}
         >
           {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="LSPD Application" />
+          <input type="hidden" name="form-name" value="contact" />
           <p hidden>
             <label>
               Don’t fill this out:{" "}
               <input name="bot-field" onChange={this.handleChange} />
             </label>
           </p>
-          <p style={{ color: 'white' }}>
+               <p style={{ color: 'white' }}>
             <label>
               Your Email:<br />
-              <input type="text" name="name" style={{ width: '75%' }} onChange={this.handleChange} />
+              <input type="text" name="Email" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
               Your Discord Username | Ex: XG.Mattis#0001:<br />
-              <input type="text" name="name" style={{ width: '75%' }} onChange={this.handleChange} />
+              <input type="text" name="Discord Username" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
               Do You Understand That Breaking Laws In Anyway Shape Of Form Will Result In Immidiate And Swift Action?:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="Law Breaking Agreement" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
              Question One: Do You Have Any Prior Experience In Law Enforcement In Anyway If so please state proof below if Not Type Negative:<br />
-              <textarea name="experience" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="experiences" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
               Question Two: How Would You Deal With A Officer Down:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="How Would You Deal With A Officer Down" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
               How Would You Deal With A Bank Robbery:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="How Would You Deal With A Bank Robbery" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
               How Would You Deal With A Pursuit:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="How Would You Deal With A Pursuit" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
               In Detail Explain Your Skills In LSPD:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="Detailed Explaination Of Their Skills In LSPD" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
               Why Do You Want To Join LSPD:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="Why Do You Want To Join LSPD" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p style={{ color: 'white' }}>
             <label>
               Why Should We Pick You Over Other Applicants 3 Sentences Minmum:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="Why Should We Pick You Over Others" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
-                <p style={{ color: 'white' }}>
+          <p style={{ color: 'white' }}>
             <label>
               What Does LSPD Do?:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="What Does LSPD Do" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
                 <p style={{ color: 'white' }}>
             <label>
               What Are Our Main Goals Here In LSPD:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="What Are LSPDS Main Goals" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
                 <p style={{ color: 'white' }}>
             <label>
              Do You Understand If You Are Found Breaking Rules, Driving around with lights on for 
              no reason Or Caught Abusing Or Disrespecting People Out Of RP you will be  Suspended:<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="Rule/TOS Agreement" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
-                <p style={{ color: 'white' }}>
+          <p style={{ color: 'white' }}>
             <label>
               Do you understand You Can Be Suspended From LSPD For Disrespecting HRS:<br />
               <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
@@ -140,12 +140,12 @@ export default class Contact extends React.Component {
                 <p style={{ color: 'white' }}>
             <label>
               Thank You For Applying For LSPD Your Results Will Be With You Within 24-48 Hours State Yes To Submit!<br />
-              <textarea name="position" style={{ width: '75%' }} onChange={this.handleChange} />
+              <textarea name="Thanks For Applying" style={{ width: '75%' }} onChange={this.handleChange} />
             </label>
           </p>
           <p>
             <button type="submit">Send</button>
-          </p>
+            </p>
         </form>
       </div>
     );
