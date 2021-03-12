dab1cd31c6ac8318f7dc98f0f989a435321c7421
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-12 00:03:33 +0000

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,16 +9,16 @@ export default class Index extends React.Component {
     return (
       <div>
         <p style={{ color: 'white' }}>
-          If you're interested in Aplying to be a member of one of our departments? Appeal a ban or anything else you're in the right place, Please choose from one of the Forms Below
+          If you're interested in Aplying to be a member of one of our Serivces?? Appeal a ban or anything else you're in the right place, Please choose from one of the Forms Below
         </p>
         <ul>
           <li style={{ color: '#8A6AFD' }}><Link to="/staffapps/" style={{ color: '#00B0E1' }}>Staff Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/devapps/" style={{ color: '#00B0E1' }}>Developer Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/depthead/" style={{ color: '#00B0E1' }}>Department Head Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/sahpapps/" style={{ color: '#00B0E1' }}>SAHP Application</Link></li>
-          <li style={{ color: '#8A6AFD' }}><Link to="/bcsoapps/" style={{ color: '#00B0E1' }}>BCSO Application</Link></li>
-          <li style={{ color: '#8A6AFD' }}><Link to="/lspdapps/" style={{ color: '#00B0E1' }}>LSPD Application</Link></li>
-          <li style={{ color: '#8A6AFD' }}><Link to="/safdapps/" style={{ color: '#00B0E1' }}>SAFD Application</Link></li>
+          <li style={{ color: '#8A6AFD' }}><Link to="/metpsapps/" style={{ color: '#00B0E1' }}>BCSO Application</Link></li>
+          <li style={{ color: '#8A6AFD' }}><Link to="/nhsapps/" style={{ color: '#00B0E1' }}>LSPD Application</Link></li>
+          <li style={{ color: '#8A6AFD' }}><Link to="/lfbapps/" style={{ color: '#00B0E1' }}>SAFD Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/appeals/" style={{ color: '#00B0E1' }}>Ban Appeals</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/bugreport/" style={{ color: '#00B0E1' }}>Bug Report</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/suggestions/" style={{ color: '#00B0E1' }}>Suggestions</Link></li>
