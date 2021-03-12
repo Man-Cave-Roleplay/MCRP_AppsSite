03739cae4083e19e66b3b34d42f35b13734cfcac
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-12 04:45:05 +0000

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,11 @@ export default class Index extends React.Component {
           <li style={{ color: '#8A6AFD' }}><Link to="/depthead/" style={{ color: '#00B0E1' }}>Department Head Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/nhsapps/" style={{ color: '#00B0E1' }}>NHS Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/metpsapps/" style={{ color: '#00B0E1' }}>Metropolitan Police Application</Link></li>
-          <li style={{ color: '#8A6AFD' }}><Link to="/lfbapps/" style={{ color: '#00B0E1' }}>LFB Application</Link></li>        
+          <li style={{ color: '#8A6AFD' }}><Link to="/lfbapps/" style={{ color: '#00B0E1' }}>LFB Application</Link></li> 
+           <li style={{ color: '#8A6AFD' }}><Link to="/suggestions/" style={{ color: '#00B0E1' }}>Suggestions</Link></li>        
+          <li style={{ color: '#8A6AFD' }}><Link to="/bugreports/" style={{ color: '#00B0E1' }}>Bug Reports</Link></li>        
+           <li style={{ color: '#8A6AFD' }}><Link to="/staffreports/" style={{ color: '#00B0E1' }}>Staff Reports</Link></li>        
+
       </ul>
       </div>
     );
