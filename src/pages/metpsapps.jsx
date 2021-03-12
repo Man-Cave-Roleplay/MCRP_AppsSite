f854e951be614731cd288fc78100a122ba220ed6
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-12 04:43:23 +0000

    Update metpsapps.jsx

--- src/pages/metpsapps.jsx
+++ src/pages/metpsapps.jsx
@@ -35,7 +35,7 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div>
-        <h1 style={{ color: 'white' }}>met Application</h1>
+        <h1 style={{ color: 'white' }}>Metropolitan Police Application</h1>
         <form
           name="metpsapps"
           method="post"
