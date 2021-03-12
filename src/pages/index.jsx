4bc3ae798020f695c8bcd3abf2bcde7d114c2085
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-12 01:36:45 +0000

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,10 +15,9 @@ export default class Index extends React.Component {
           <li style={{ color: '#8A6AFD' }}><Link to="/staffapps/" style={{ color: '#00B0E1' }}>Staff Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/devapps/" style={{ color: '#00B0E1' }}>Developer Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/depthead/" style={{ color: '#00B0E1' }}>Department Head Application</Link></li>
-          <li style={{ color: '#8A6AFD' }}><Link to="/nhsapps/" style={{ color: '#00B0E1' }}>NHSApplication</Link></li>
+          <li style={{ color: '#8A6AFD' }}><Link to="/nhsapps/" style={{ color: '#00B0E1' }}>NHS Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/metpsapps/" style={{ color: '#00B0E1' }}>Metropolitan Police Application</Link></li>
-          <li style={{ color: '#8A6AFD' }}><Link to="/lfbapps/" style={{ color: '#00B0E1' }}>LFB Application</Link></li>
-        </ul>
+          <li style={{ color: '#8A6AFD' }}><Link to="/lfbapps/" style={{ color: '#00B0E1' }}>LFB Application</Link></li>        </ul>
       </div>
     );
   }
