6f2b150722f9d2d6d3efb59ff410efbc8f082471
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-12 01:37:48 +0000

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,8 @@ export default class Index extends React.Component {
           <li style={{ color: '#8A6AFD' }}><Link to="/depthead/" style={{ color: '#00B0E1' }}>Department Head Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/nhsapps/" style={{ color: '#00B0E1' }}>NHS Application</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/metpsapps/" style={{ color: '#00B0E1' }}>Metropolitan Police Application</Link></li>
-          <li style={{ color: '#8A6AFD' }}><Link to="/lfbapps/" style={{ color: '#00B0E1' }}>LFB Application</Link></li>        </ul>
+          <li style={{ color: '#8A6AFD' }}><Link to="/lfbapps/" style={{ color: '#00B0E1' }}>LFB Application</Link></li>        
+      </ul>
       </div>
     );
   }
