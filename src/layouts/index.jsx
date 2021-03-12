262d1e9d1eae2d5e086329b597e2515c8aa46f35
XavierGMattis <79116568+XGMidas@users.noreply.github.com>
2021-03-12 00:37:11 +0000

    Update index.jsx

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -19,7 +19,7 @@ export default class Template extends React.Component {
           title="Mancave Roleplay Official Applications Portal"
           meta={[
             { name: "description", content: "Interested in Apllying for a Service?, Reporting a bug or anything else of that nature? You can do so here." },
-            { name: "keywords", content: "Department Of Dedicated Roleplay, top.gg" },
+            { name: "keywords", content: "Mancave Roleplay, top.gg" },
           ]}
         />
         <div
